4d1b8a63c47db31ee13a17e5b5aab309e631d70a
protop96 <protop96@gmail.com>
2014-06-02 20:37:28 +0300

    اصلاح خطأ تسجيل الدخول  admin

--- admin/logged.php
+++ admin/logged.php
@@ -6,23 +6,27 @@ session_start();
 // protop96@gmail.com
 // http://waseethost.com/
 #################################################
-header('Location: index.php ');
+//header('Location: index.php ');
 require './../config/dbconfig.php';
-@$user=mysql_real_escape_string($_POST['username']);
-@$password=mysql_real_escape_string($_POST['password']);
+print $user = mysqli_real_escape_string($con,$_POST['username']);
+print$password = mysqli_real_escape_string($con,$_POST['password']);
 if(!$user && !$password) {
+
+}else {
+
+$sql = mysqli_query($con,"select * from `AdminCp` where `Username` = '$user' and `Password` = '$password' LIMIT 1") ;
+$row = mysqli_fetch_assoc($sql);
+print_r($row);
+//print_r($_POST);
+if($user == $row['Username']  && $password == $row['Password']){
+	$_SESSION['admin'] = 1;
     header('Location:index.php');
 }else {
-
-$sql=mysqli_query($con,"select * from AdminCp where  Username='$user' and Password='$password' ") ;
-$row=mysqli_fetch_assoc($sql);
-
-if($user==$row['Username'] && $password==$row['Password']){
-	$_SESSION['admin'] = 1;
-
-}else {
 	$_SESSION['admin'] = '';
+print_r($_POST);
+}
+}
 
-}
-}
+mysqli_close($con);
+
 ?>
